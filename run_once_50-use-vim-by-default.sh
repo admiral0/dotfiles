2fb2b3983ecfab6eea43a6ac50a10dba1d9b3d1d
Radu Andries <me@admiral0.it>
2022-07-24 16:55:17 +0200

    get rid of cruft

--- run_once_50-use-vim-by-default.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source /etc/os-release
-
-if [ ${ID} = "fedora" ]; then
-  sudo dnf install --allowerasing vim-default-editor vim-enhanced
-fi
--- run_once_after.sh
+++ run_once_after.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
 
+
 asdf plugin-add python
 asdf plugin-add rust
 asdf plugin-add awscli
 asdf plugin-add java
 asdf plugin-add protoc
+asdf plugin-add neovim
 
-
-asdf install rust stable
-asdf global rust stable
