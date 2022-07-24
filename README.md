de6952c53d0c8a72e96b29d84a2b8f97a3ba6914
Radu Andries <me@admiral0.it>
2022-07-24 18:19:03 +0200

    README

--- README.md
+++ README.md
@@ -1,26 +1,5 @@
 # My dotfiles
 
-This is what I currently set up on my computers.
-
-## System Checklist
-
-### Interface
-* i3-gaps
-* i3lock
-* rofi
-* dunst
-* polybar
-
-### Applications
-* zsh
-* alacritty
-* spotify
-* curl
-* screen
-
-
-### Fonts
-* Font Awesome
-* Open Symbol
-* Jetbrains Mono
-
+```sh
+$ sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply admiral0
+```
--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -151,6 +151,36 @@ lvim.builtin.treesitter.highlight.enabled = true
 --       cmd = "TroubleToggle",
 --     },
 -- }
+lvim.plugins = {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.g.tokyonight_style = "storm"
+      vim.g.tokyonight_sidebars = {
+        "qf",
+        "vista_kind",
+        "terminal",
+        "packer",
+        "spectre_panel",
+        "NeogitStatus",
+        "help",
+      }
+      vim.g.tokyonight_cterm_colors = false
+      vim.g.tokyonight_terminal_colors = true
+      vim.g.tokyonight_italic_comments = false
+      vim.g.tokyonight_italic_keywords = true
+      vim.g.tokyonight_italic_functions = false
+      vim.g.tokyonight_italic_variables = false
+      vim.g.tokyonight_transparent = lvim.transparent_window
+      vim.g.tokyonight_hide_inactive_statusline = true
+      vim.g.tokyonight_dark_sidebar = true
+      vim.g.tokyonight_dark_float = true
+      vim.g.tokyonight_dim_inactive = true
+      vim.g.tokyonight_global_status = true
+      vim.cmd [[colorscheme tokyonight]]
+    end
+  }
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
