13cdcdb503f44849f825bfefb7cd885cc47f604f
Radu Andries <me@admiral0.it>
2022-07-24 17:02:32 +0200

    add latest changes to zshrc

--- run_once_after.sh
+++ run_once_after.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
-
+# Take care of tools
+asdf plugin-add poetry
 asdf plugin-add python
 asdf plugin-add rust
 asdf plugin-add awscli
 asdf plugin-add java
 asdf plugin-add protoc
 asdf plugin-add neovim
+asdf plugin-add golang
 
+asdf install
--- run_once_before.sh
+++ run_once_before.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
