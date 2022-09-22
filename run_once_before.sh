0037ea49f7b2f74c3172637f884e5b01fe4a05a1
Radu Andries <me@admiral0.it>
2022-09-22 13:47:54 +0200

    remove spaceship prompt

--- run_once_before.sh
+++ run_once_before.sh
@@ -6,9 +6,3 @@ if [ ! -d $HOME/.oh-my-zsh/ ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-
-export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
-if [ ! -d  $ZSH_CUSTOM/themes/spaceship-prompt ]; then
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-  ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-fi
